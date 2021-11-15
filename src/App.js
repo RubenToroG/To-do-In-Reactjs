1af29bce0fd3733942bda73ed3c18a409291177a
Ruben Toro <rubensdt01@gmail.com>
2021-11-15 16:15:33 -0500

    Se agrega estado en componente de contador y Search, ya funciona la busqueda de TODOs

--- src/App.js
+++ src/App.js
@@ -6,21 +6,46 @@ import { TodoSearch } from './TodoSearch.js';
 import { CreateTodoButton } from './CreateTodoButton.js';
 
 //import './App.css';
-const todos = [
+const defaultTodos = [
   { text: 'Cortar Cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Tomar el curso de intro a React', completed: true },
   { text: 'Compra mes', completed: true },
   { text: 'Compra semana', completed: false }
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length; //filtrando los todos para ver cuales tienen la propiedad completed como true, !!
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+    
+  }
+
   return (
     <React.Fragment>  {/*renderiza una etiqueta invisible para envolver a todos los componentes*/}
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+      total={totalTodos}
+      completed={completedTodos}
+      />
+      <TodoSearch 
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,19 @@
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch () {
+function TodoSearch ({ searchValue, setSearchValue }) {
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     };
 
     return (
         <input placeholder="Busqueda Avanzada" 
         className="TodoSearch"
+        value={searchValue}
         onChange={onSearchValueChange}
         />
-    );
+        );
 }
 
 export { TodoSearch };
