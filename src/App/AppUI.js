09cdf0caa58dc8fc1eb54eb65e200af5bf6f3348
Ruben Toro <rubensdt01@gmail.com>
2021-11-28 01:20:14 -0500

    Se implemento React Context

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,47 +1,41 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext/index.js';
 import { TodoCounter } from '../TodoCounter/index.js';
 import { TodoList } from '../TodoList/index.js';
 import { TodoItem } from '../TodoItem/index.js';
 import { TodoSearch } from '../TodoSearch/index.js';
 import { CreateTodoButton } from '../CreateTodoButton/index.js';
 
-function AppUI({ 
-    loading,
-    error,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
- }) {
+function AppUI() {
+
+    const {
+        error,
+        loading,
+        searchedTodos,
+        completeTodo,
+        deleteTodo
+    } = React.useContext(TodoContext);
+
     return (
         <React.Fragment>  {/*renderiza una etiqueta invisible para envolver a todos los componentes*/}
-            <TodoCounter
-                total={totalTodos}
-                completed={completedTodos}
-            />
-            <TodoSearch
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-            />
+            <TodoCounter />
+            <TodoSearch />
 
-            <TodoList>
-                {error && <p>Hubo un error...</p>}
-                {loading && <p>Cargando...</p>}
-                {(!loading && !searchedTodos.lenth) && <p>Crea tu primer TODO...</p>}
+                    <TodoList>
+                        {error && <p>Hubo un error...</p>}
+                        {loading && <p>Cargando...</p>}
+                        {(!loading && !searchedTodos.lenth) && <p>Crea tu primer TODO...</p>}
 
-                {searchedTodos.map(todo => (
-                    <TodoItem
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodo(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                    />
-                ))}
-            </TodoList>
+                        {searchedTodos.map(todo => (
+                            <TodoItem
+                                key={todo.text}
+                                text={todo.text}
+                                completed={todo.completed}
+                                onComplete={() => completeTodo(todo.text)}
+                                onDelete={() => deleteTodo(todo.text)}
+                            />
+                        ))}
+                    </TodoList>
 
             <CreateTodoButton />
 
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import './TodoSearch.css';
 
-function TodoSearch ({ searchValue, setSearchValue }) {
+function TodoSearch() {
+    const { searchValue, setSearchValue } = React.useContext(TodoContext);
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value)
     };
 
     return (
-        <input placeholder="Busqueda Avanzada" 
-        className="TodoSearch"
-        value={searchValue}
-        onChange={onSearchValueChange}
+        <input placeholder="Busqueda Avanzada"
+            className="TodoSearch"
+            value={searchValue}
+            onChange={onSearchValueChange}
         />
-        );
+    );
 }
 
 export { TodoSearch };
